ae7eb88391c040d77fb1663dfddf64009315ccbf
prateekYadav07 <yadavprateek39@gmail.com>
2024-07-23 22:41:10 +0530

    first commit

--- src/main/java/com/masterclass/arrays/ArraysUtilityClass.java
+++ src/main/java/com/masterclass/arrays/ArraysUtilityClass.java
@@ -1,18 +1,42 @@
 package com.masterclass.arrays;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Random;
 
 public class ArraysUtilityClass {
 
     public static void main(String[] args) {
-        int[] arr = getRandomArray(10);
-        System.out.println("Unsorted array" + Arrays.toString(arr));
-        int[] revArr = reverseSort(arr);
-        System.out.println("Decreasing order sorted array" + Arrays.toString(revArr));
-        Arrays.sort(arr);
-        System.out.println("Increasing order sorted array" + Arrays.toString(arr));
+//        int[] arr = getRandomArray(10);
+//        System.out.println("Unsorted array" + Arrays.toString(arr));
+//        int[] revArr = reverseSort(arr);
+//        System.out.println("Decreasing order sorted array" + Arrays.toString(revArr));
+//        Arrays.sort(arr);
+//        System.out.println("Increasing order sorted array" + Arrays.toString(arr));
+
+        // Arrays.equals and deepEquals
+        String[] intro = new String[]{"once", "upon", "a", "time" };
+        System.out.println("single level of equality with equals "+ Arrays.equals(intro, new String[]{"once", "upon", "a", "time" }));
+        Object[] copy = new Object[]{intro, new String[] {"One", "Two"}};
+        System.out.println("Deep comparison " + Arrays.deepEquals(copy, new Object[]{intro, new String[] {"One", "Two"}}));
+
+        System.out.println(intro.hashCode()+ " " + Arrays.hashCode(intro));
+
+        String[] outro = Arrays.copyOf(intro, intro.length);
+        System.out.println("intro: " + Arrays.toString(intro) + " outro: " + Arrays.toString(outro));
+        System.out.println("intro hashcode: "+ Arrays.hashCode(intro) + " outros hashcode: "+ Arrays.hashCode(outro));
+        System.out.println("intro hashcode directly: "+ intro.hashCode() + " outros hashcode directly: "+ outro.hashCode());
+
+        // sort and parallelSort
+        System.out.println("++++++ Sort Comparison ++++++++");
+        int[] arr1 = getRandomArray(10000000);
+        long startTime = System.nanoTime();
+        Arrays.sort(Arrays.copyOf(arr1, arr1.length));
+        System.out.println("sort took: "+ (System.nanoTime() - startTime));
+
+        long parallelStart = System.nanoTime();
+        Arrays.parallelSort(Arrays.copyOf(arr1, arr1.length));
+        System.out.println("parallel sort took: "+ (System.nanoTime() - parallelStart));
+
     }
 
     public static int[] getRandomArray(int len){
@@ -20,7 +44,7 @@ public class ArraysUtilityClass {
         Random random = new Random();
 
         for(int i = 0; i<len; i++){
-            retArr[i] = random.nextInt(100);
+            retArr[i] = random.nextInt(1000);
         }
 
         return retArr;
